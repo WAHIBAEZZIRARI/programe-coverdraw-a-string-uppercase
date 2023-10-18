47336b84e6117f486394b950b5defe5e63531043
WAHIBAEZZIRARI <146207125+WAHIBAEZZIRARI@users.noreply.github.com>
2023-10-18 14:38:47 +0100

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,6 @@
+def convertir_en_majuscules(chaine):
+    return chaine.upper()
+
+chaine = input("Entrez une chaîne de caractères : ")
+chaine_en_majuscules = convertir_en_majuscules(chaine)
+print("La chaîne en majuscules est :", chaine_en_majuscules)
